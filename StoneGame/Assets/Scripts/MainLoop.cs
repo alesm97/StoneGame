e9f50209821e809bedb1fea4f1f45be5741239e1
alesm97 <32259637+alesm97@users.noreply.github.com>
2018-01-17 13:45:03 +0100

    13/01/2018 13:44

--- StoneGame/Assets/Scripts/MainLoop.cs
+++ StoneGame/Assets/Scripts/MainLoop.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,15 +12,42 @@ public class MainLoop : MonoBehaviour {
     public float minTimeBetweenStones = 1f, maxTimeBetweenStones = 3f;
     public float minX = -30f, maxX = 30f;
     public float minZ = -5f, maxZ = 5f;
-    //public float minX = -30f, maxX = 30f;
+
+    private bool enableStones = true;
+    private Rigidbody rigidbody;
 
     // Use this for initialization
     void Start () {
+        StartCoroutine(ThrowStones());
+	}
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private IEnumerator ThrowStones()
+    {
+
+        yield return new WaitForSeconds(2);
+
+        while (enableStones)
+        {
+            GameObject stone = Instantiate(stones[UnityEngine.Random.Range(0, stones.Length)]);
+            stone.transform.position = new Vector3(UnityEngine.Random.Range(minX, maxX), -30, UnityEngine.Random.Range(minZ, maxZ));
+            stone.transform.rotation = UnityEngine.Random.rotation;
+
+            rigidbody = stone.GetComponent<Rigidbody>();
+
+            rigidbody.AddTorque(Vector3.up * torque, ForceMode.Impulse);
+            rigidbody.AddTorque(Vector3.right * torque, ForceMode.Impulse);
+            rigidbody.AddTorque(Vector3.forward * torque, ForceMode.Impulse);
+
+            rigidbody.AddForce(Vector3.up * UnityEngine.Random.Range(minAntiGravity, maxAntiGavity), ForceMode.Impulse);
+            rigidbody.AddForce(Vector3.right * UnityEngine.Random.Range(minLateralForce, maxLateralForce), ForceMode.Impulse);
+
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minTimeBetweenStones, maxTimeBetweenStones));
+        }
+        
+    }
 }
